c9fe0cd402d2759074d0b3d4bd8171d8c7d80012
steima <matthias.steinbauer@gmail.com>
2013-05-31 09:38:09 +0200

    Added an update script

---
+++ php-kurskoffer/update.php
@@ -0,0 +1,12 @@
+<?php	
+	$notificationFile = '/tmp/updateKurskoffer';
+	$handle = fopen($notificationFile, 'w') or die('Cannot open file: ' . $notificationFile);
+?>
+<html>
+	<head>
+		<title>Update Status</title>
+	</head>
+	<body>
+		Notification File: <?php echo $notificationFile; ?> was written, will be picked up in a minute.
+	</body>
+</html>
