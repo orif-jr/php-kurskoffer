7fc3cca1975ebaa773d6a7e5f44e6d84ff3fbc18
steima <matthias.steinbauer@gmail.com>
2013-06-05 08:53:11 +0200

    Added a getProgress.php

---
+++ php-kurskoffer/getProgress.php
@@ -0,0 +1,23 @@
+<?php
+	error_reporting(E_ALL);
+	/* Connect to the database */
+	include_once ("dbsettings.php");
+
+	if ( isset($_GET['username']) ){
+		$sql = "select uid from accounts where login like '" . $_POST['username'] . "'";
+		$result = mysql_query($sql);
+		$row = mysql_fetch_assoc($result);
+		if($row) {
+			$uid = $row['uid'];
+			$sql = 'select count(*) scount from progress where uid = ' . $uid;
+			$result = mysql_query($sql);
+			$row = mysql_fetch_assoc($result);
+			if($row) {
+				$scount = $row['scount'];
+				// TODO topic count hard coded!
+				$progress = array('readTopics' => $scount, 'topicCount' => 19);
+				echo json_encode($progress);
+			}
+		}
+	}
+?>
